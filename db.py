bea5396f5bb3572ea7f1b1840d08d5197126e7ea
bashayeralhumaidi <bashayer.alhumaidi@gmail.com>
2026-02-18 22:53:02 +0400

    Update db.py

--- db.py
+++ db.py
@@ -1,13 +1,10 @@
-import pyodbc
+import pymssql
+import os
 
 def get_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=confirmaapplication.database.windows.net,1433;"
-        "DATABASE=voting;"
-        "UID=Julphar_Admin;"
-        "PWD=Bashayer01;"
-        "Encrypt=yes;"
-        "TrustServerCertificate=no;"
-        "Connection Timeout=30;"
+    return pymssql.connect(
+        server="voting01.database.windows.net",
+        user=os.environ["Julphar_Admin"],
+        password=os.environ["Bashayer01"],
+        database="Voting"
     )
