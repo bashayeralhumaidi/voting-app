6b08f7be40cc9fef6d79a98646ea7b7f8c2745b2
bashayeralhumaidi <bashayer.alhumaidi@gmail.com>
2026-03-08 04:02:27 +0400

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
 import pymssql
 import os
 
-def get_connection():
-    return pymssql.connect(
-        server=os.environ["DB_SERVER"],
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASSWORD"],
-        database=os.environ["DB_NAME"],
-        charset="UTF-8"
-    )
+conn = pymssql.connect(
+    server=os.environ["DB_SERVER"],
+    user=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"],
+    database=os.environ["DB_NAME"],
+    charset="UTF-8"
+)
 
+def get_connection():
+    return conn
